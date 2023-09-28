ad98cd2343c63ab13055bcd0396beaadd3fa8842
Emmanuel-Mingala <emmanuel.mingala@tup.edu.ph>
2023-09-28 15:10:24 +0800

    routes

---
+++ routes/post.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+//import controller methods
+const {create} = require('../controllers/post');
+router.post('/post', create);
+
+module.exports = router;
